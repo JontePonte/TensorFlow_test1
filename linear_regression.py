d65b020ef0908008ab512e906492046dfdb0c9da
JontePonte <john_nordstrand@hotmail.com>
2019-09-25 21:38:42 +0200

    More stuff in main file

--- linear_regression.py
+++ linear_regression.py
@@ -1,7 +1,7 @@
 
 import tensorflow as tf
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 # define variables
 learning_rate = 0.01
@@ -10,4 +10,7 @@ epochs = 200
 # Create training data
 n_samples = 30
 train_x = np.linspace(0, 20, n_samples)
+train_y = 3*train_x + 4*np.random.randn(n_samples)
 
+plt.plot(train_x, train_y)
+plt.show()
