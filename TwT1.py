fb62edb4111e3fa7c6b8917de5b48cf2ada34217
JontePonte <john_nordstrand@hotmail.com>
2019-09-27 10:04:21 +0200

    Data import working

--- TwT1.py
+++ TwT1.py
@@ -1,4 +1,22 @@
 
-"""
-First example from Tech with Tim
-"""
+""" First example from Tech with Tim """
+
+# imports
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow import keras
+
+
+""" Import data in a training set and a test set """
+
+data = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+plt.imshow(train_images[7])
+plt.show()
+
