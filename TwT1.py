459bac51f2bb0e2d66618b45b451efe0cc5bbf6c
JontePonte <john_nordstrand@hotmail.com>
2019-09-27 12:56:33 +0200

    It works! Model get test_data acc at 87 %

--- TwT1.py
+++ TwT1.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 
 from tensorflow import keras
 
+is_showimage = False
+epochs_num = 5
 
 """ Import data in a training set and a test set """
 
@@ -22,6 +24,23 @@ train_images = train_images / 255.0
 test_images = test_images / 255.0
 
 # Show image number 7 as example
-plt.imshow(train_images[7], cmap=plt.cm.binary)
-plt.show()
+if is_showimage:
+    plt.imshow(train_images[7], cmap=plt.cm.binary)
+    plt.show()
 
+
+""" Create structure for the Neural Network """
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+# Set optimizer, loss function and correction metrics
+model.compile(optimmizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=epochs_num)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print("Tested Acc:", test_acc)
