2c6e6aed678be4faa58ab99007cd065d35fc9126
JontePonte <john_nordstrand@hotmail.com>
2019-09-29 22:50:16 +0200

    Second video on movie review NN is done. It seems to be working

--- TwT2.py
+++ TwT2.py
@@ -4,14 +4,19 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
+""" Variables """
+epochs_nr = 40
+batch_size_nr = 512
+show_nr = 0
+
 """ Import data in train and test sets"""
 data = keras.datasets.imdb
 (train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
 
 # Show the structure of the data set
-print(train_data[0])
+print(train_data[show_nr])
 
-# Fix the data set index
+""" Fix the data so it can be used in the NN """
 word_index = data.get_word_index()
 word_index = {k: (v+3) for k, v in word_index.items()}
 word_index["<PAD>"] = 0         # THe data will be padded so that every input has the same length
@@ -23,8 +28,10 @@ word_index["<UNUSED>"] = 3
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
 # Pad the data with fancy tf function so that every text has the same length.
-train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], padding="post", maxlen=256)
-test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_index["<PAD>"], padding="post", maxlen=256)
+train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"],
+                                                        padding="post", maxlen=256)
+test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_index["<PAD>"],
+                                                       padding="post", maxlen=256)
 
 # Function that decode input data to text
 def decode_review(text):
@@ -32,7 +39,7 @@ def decode_review(text):
 
 
 # Print text instead of number coded data
-print(decode_review(test_data[0]))
+print(decode_review(test_data[show_nr]))
 
 
 """ Define the model structure """
@@ -43,3 +50,27 @@ model.add(keras.layers.Dense(16, activation="relu"))
 model.add(keras.layers.Dense(1, activation="sigmoid"))
 
 model.summary()
+
+# Set optimizer and loss function
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+x_val = train_data[:10000]
+x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+y_train = train_labels[10000:]
+
+"""" Train the model and evaluate the results """
+fit_model = model.fit(x_train, y_train, epochs=epochs_nr, batch_size=batch_size_nr,
+                      validation_data=(x_val, y_val), verbose=1)
+
+results = model.evaluate(test_data, test_labels)
+
+# Show a review, the label and the predicted label
+test_review = test_data[show_nr]
+predict = model.predict([test_review])
+print("Review: ")
+print(decode_review(test_review))
+print("Prediction: ", str(predict[show_nr]))
+print("Actial: ", str(test_labels[show_nr]))
+print(results)
