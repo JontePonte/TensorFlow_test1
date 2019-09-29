1d2d6e811112cb1b2778dcac11dddb3e29dbbdd2
JontePonte <john_nordstrand@hotmail.com>
2019-09-29 22:23:23 +0200

    Fixed typos

--- TwT2.py
+++ TwT2.py
@@ -37,8 +37,8 @@ print(decode_review(test_data[0]))
 
 """ Define the model structure """
 model = keras.Sequential()
-model.add(keras.layers.Embedded(10000, 16))
-model.add(keras.layers.GlobalAveragePoolingID())
+model.add(keras.layers.Embedding(10000, 16))
+model.add(keras.layers.GlobalAveragePooling1D())
 model.add(keras.layers.Dense(16, activation="relu"))
 model.add(keras.layers.Dense(1, activation="sigmoid"))
 
